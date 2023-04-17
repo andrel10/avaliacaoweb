33975d05f0668a34fd0f14dc3e1879fc707247ce
André <andre.l10@aluno.ifsc.edu.br>
2023-04-17 20:15:47 -0300

    arquivos versionados

---
+++ rotas.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const routes = express.Router()
+
+let lista = [{"id":1, "descricao":"Camiseta", "preço":20.99, "cores":["preto","branco","cinza"]}, {"id":2, "descricao":"Calça jeans", "preco":79.99, "cores":["preto, Branco, azul escuro"]}, {"id":3, "descricao":"Tênis esportivo", "preco":149.99, "cores":["preto, marrom"]}]
+
+routes.get("/",(req, res)=>{
+    res.status(200).json(lista)
+})
+
+routes.get("/:id",(req, res)=>{
+    res.status(200).json(lista[req.params.id-1])
+})
+
+routes.delete("/:id",(req, res)=>{
+    lista.splice(req.params.id-1,1)
+    res.status(200).redirect("/")
+})
+
+routes.put("/:id",(req, res)=>{
+
+})
+
+module.exports = routes
