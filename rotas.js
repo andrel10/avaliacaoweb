062b2cb197e075dcdf1b468889bd46f2d74d4bfb
André <andre.l10@aluno.ifsc.edu.br>
2023-04-17 22:06:41 -0300

    comitando a ultima vez

--- rotas.js
+++ rotas.js
@@ -2,36 +2,45 @@ const express = require("express")
 
 const routes = express.Router()
 
+let indice = 4
+
 var lista = [{"id": 1, "descricao":"Camiseta", "preço":20.99, "cores":["preto","branco","cinza"]},
 {"id": 2, "descricao":"Calça jeans", "preco":79.99, "cores":["preto, Branco, azul escuro"]},
 {"id": 3, "descricao":"Tênis esportivo", "preco":149.99, "cores":["preto, marrom"]},
 {"id": 4, "descricao": "Jaqueta de Couro", "preco": 99.99, "cores": ["preto", "marrom"]}
 ]
 
-routes.getAll("/",(req, res)=>{
+routes.get("/",(req, res)=>{
     res.status(200).json(lista)
 })
 
-routes.getById("/:id",(req, res)=>{
+routes.get("/:id",(req, res)=>{
     // res.status(200).json(lista[req.params.id-1])
     causa = lista.find(i => i.id == req.params.id)  
     if(causa){
-      res.status(200).send(causa)
+        res.status(200).json(lista[req.params.id-1])
 
     } else{
-      res.status(404).console.log("servidor fora de ar!!")
+      res.status(404).send("servidor fora de ar!!")
     }
   }
 )
 
 routes.delete("/:id",(req, res)=>{
     lista.splice(req.params.id-1,1)
-    res.status(200).redirect("/")
+    res.status(200).send("OK")
 })
 
 routes.post("/",(req, res)=>{
     lista.push(req.body)
     res.status(200).redirect("/")
+}
+)
+
+routes.put("/:id",(req, res)=>{
+    console.log(req.body)
+    lista[req.params.id-1] = req.body
+    res.status(200).send("OK")
 })
 
 module.exports = routes
--- servidor.js
+++ servidor.js
@@ -3,9 +3,10 @@ const routes = require("./rotas")
 
 const server = express()
 
-server.use(routes)
+
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
+server.use(routes)
 
 server.listen(3000, ()=>{
     console.log("Servidor em execução...")
